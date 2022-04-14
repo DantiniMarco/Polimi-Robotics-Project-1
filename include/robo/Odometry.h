b4103737137aff2a231777188274760813341b5b
DantiniMarco <10603556@polimi.it>
2022-04-14 16:32:33 +0000

    added service,  parameters and integration methods

--- include/robo/Odometry.h
+++ include/robo/Odometry.h
@@ -4,7 +4,11 @@
 #include "ros/ros.h"
 #include "sensor_msgs/JointState.h" // wheel_states messages
 #include "geometry_msgs/TwistStamped.h" // published messages
-
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <dynamic_reconfigure/server.h>
+#include <Odometry/parametersConfig.h>
 #include <math.h>
 
 // Publish v, ⍵ as topic cmd_vel of type geometry_msgs/TwistStamped
@@ -12,7 +16,8 @@
 class Odometry {
 private:
     const double l = 0.2, w = 0.169, r = 0.07, gear_ratio = 5.0;
-    double omega, vx, vy;
+    double omega, vx, vy, new_x, new_y, new_theta;
+    int integration_method;
 
     /*
       Front left wheel = 1
@@ -22,16 +27,29 @@ private:
     */
     double w1, w2, w3, w4; // input in [rad/min]
 
-    geometry_msgs::TwistStamped velocities; // message to be published
+    geometry_msgs::TwistStamped velocities; // v and w velocities computed from wheel speeds to be published
+
+    nav_msgs::Odometry odometry_msg; //computed odometry to be published 
+
+    geometry_msgs::TransformStamped transformStamped;
 
     float t_s; //time of sampling
     ros::Time current_time = ros::Time(0);
     ros::Time latest_sent_time = ros::Time(0);
 
     ros::Subscriber sub;
-    ros::Publisher pub;
+    ros::Publisher pub_speeds;
+    ros::Publisher pub_odom;
     ros::NodeHandle node;
     ros::Timer timer;
+    ros::ServiceServer serviceSet;
+
+    tf2::Quaternion current_quaternion;
+    tf2_ros::TransformBroadcaster br;
+
+    dynamic_reconfigure::Server<Odometry::parametersConfig> server;
+    dynamic_reconfigure::Server<Odometry::parametersConfig>::CallbackType callback_f;
+
 
 
 public:
--- src/Odometry.cpp
+++ src/Odometry.cpp
@@ -1,12 +1,25 @@
-#include "robo/main.h"
+#include "robo/Odometry.h"
 
 Odometry::Odometry() {
 
     sub = node.subscribe("wheel_states", 1000, &Odometry::wheel_state_callback, this);
 
-    pub = node.advertise<geometry_msgs::TwistStamped>("/cmd_vel", 1);
+    pub_speeds = node.advertise<geometry_msgs::TwistStamped>("/cmd_vel", 1);
+
+    pub_odom = node.advertise<nav_msgs::Odometry>("/odom", 1);
 
     timer = node.createTimer(ros::Duration(0.01), &Odometry::callback_publisher_timer, this);
+
+    callback_f = boost::bind(&Odometry::callback_dynamic_reconfigure, this, _1, _2);
+    server.setCallback(callback_f);
+
+
+    serviceSet = node.advertiseService<Odometry::Reset::Request, 
+                         Odometry::Reset::Response>("set_odom",
+                         boost::bind(&callback_set_odom, &new_x, &new_y, &new_theta, _1, _2, _3));
+
+
+
 
     // robot/pose
     //msg->pose.position.//x,y,z;
@@ -16,7 +29,7 @@ Odometry::Odometry() {
 
 void Odometry::wheel_state_callback(const sensor_msgs::JointStateConstPtr& msg) {
     if (current_time == ros::Time(0)) {
-        //current_quaternion.setRPY(0,0,0);
+        current_quaternion.setRPY(0,0,0);
     }
     current_time = msg->header.stamp;
 
@@ -27,13 +40,13 @@ void Odometry::wheel_state_callback(const sensor_msgs::JointStateConstPtr& msg) 
 
     computeVelocities();
 
-    ROS_INFO("suca %d", msg->header.seq);
+    //ROS_INFO("say %d", msg->header.seq);
 
     velocities.twist.linear.x = vx;
     velocities.twist.linear.y = vy;
     velocities.twist.angular.z = omega;
 
-    //pub.publish(velocities);
+    //pub_speeds.publish(velocities);
 }
 
 void Odometry::publisher() {
@@ -44,9 +57,24 @@ void Odometry::publisher() {
 void Odometry::callback_publisher_timer(const ros::TimerEvent& ev) {
     if ((latest_sent_time - current_time).toSec() != 0) {
         latest_sent_time = current_time;
-        pub.publish(velocities);
+        pub_speeds.publish(velocities);
+        pub_odom.publish(custom_odometry);
     }
 }
+
+bool callback_set_odom(Odometry::set_odometry::Request &request,Odometry::set_odometry::Response &response){
+
+    new_x = request.x;
+    new_y = request.y;
+    new_theta = request.theta;
+    reset = true;
+    return true;
+    
+    }
+
+    void callback_dynamic_reconfigure(Odometry::parametersConfig &config, uint32_t level){
+        integration_method = config.integration_method;
+    }
 
 /*
 * omega: angular velocity
@@ -75,22 +103,28 @@ void Odometry::computeVelocities() {
     omega = (-w1 + w2 + w3 - w4) * (r/4.0 / (l+w)) / 60.0 / gear_ratio;
 
 }
-/*
+
 void Odometry::integrations() {
+
+    double vel = math::sqrt(vx*vx + vy*vy);
+
     if (integration_method == 1){ //Euler
         time_difference = msg->header.stamp - current_time;
         t_s = time_difference.toSec();
 
         //formulas
-        current_x = current_x + msg->twist.linear.x * t_s * cos(current_theta);
-        current_y = current_y + msg->twist.linear.x * t_s * sin(current_theta);
+        current_x = current_x + vel * t_s * cos(current_theta);
+        current_y = current_y + vel * t_s * sin(current_theta);
         current_theta = current_theta + msg->twist.angular.z * t_s;
 
-        current_quaternion.setRPY(0,0,current_theta);
+        current_quaternion.setRPY(0, 0, current_theta);
 
         custom_odometry.odom.header.stamp = msg->header.stamp;
         custom_odometry.odom.pose.pose.position.x = current_x;
         custom_odometry.odom.pose.pose.position.y = current_y;
+
+        custom_odometry.odom.pose.pose.orientation.x = current_quaternion[0];
+        custom_odometry.odom.pose.pose.orientation.y = current_quaternion[1];
         custom_odometry.odom.pose.pose.orientation.z = current_quaternion[2];
         custom_odometry.odom.pose.pose.orientation.w = current_quaternion[3];
         custom_odometry.method.data = "euler";
@@ -102,8 +136,8 @@ void Odometry::integrations() {
         t_s = time_difference.toSec();
 
         //formulas
-        current_x = current_x + msg->twist.linear.x * t_s * cos(current_theta + msg->twist.angular.z * t_s /2);
-        current_y = current_y + msg->twist.linear.x * t_s * sin(current_theta + msg->twist.angular.z * t_s /2);
+        current_x = current_x + vel * t_s * cos(current_theta + msg->twist.angular.z * t_s / 2.0);
+        current_y = current_y + vel * t_s * sin(current_theta + msg->twist.angular.z * t_s / 2.0);
         current_theta = current_theta + msg->twist.angular.z * t_s;
 
 
@@ -112,14 +146,34 @@ void Odometry::integrations() {
         custom_odometry.odom.header.stamp = msg->header.stamp;
         custom_odometry.odom.pose.pose.position.x = current_x;
         custom_odometry.odom.pose.pose.position.y = current_y;
+
+        custom_odometry.odom.pose.pose.orientation.x = current_quaternion[0];
+        custom_odometry.odom.pose.pose.orientation.y = current_quaternion[1];
         custom_odometry.odom.pose.pose.orientation.z = current_quaternion[2];
         custom_odometry.odom.pose.pose.orientation.w = current_quaternion[3];
-        custom_odometry.method.data = "rk";
+        custom_odometry.method.data = "runge-kutta";
 
         current_time = msg->header.stamp;
     }
+
+    // set header
+    transformStamped.header.stamp = current_time;
+    transformStamped.header.frame_id = "odom";
+    transformStamped.child_frame_id = "base_link";
+    // set x,y
+    transformStamped.transform.translation.x = current_x;
+    transformStamped.transform.translation.y = current_y;
+    transformStamped.transform.translation.z = 0.0;
+    // set theta
+    transformStamped.transform.rotation.x = current_quaternion.x();
+    transformStamped.transform.rotation.y = current_quaternion.y();
+    transformStamped.transform.rotation.z = current_quaternion.z();
+    transformStamped.transform.rotation.w = current_quaternion.w();
+    // send transform
+    br.sendTransform(transformStamped);
+
+
 }
-*/
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "odometry_computation");
